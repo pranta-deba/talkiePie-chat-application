8ce8ff8dfee68c4293fa9c275845b9fdd45310d5
Pranto Deb <rajprit0m@outlook.com>
2025-03-15 21:08:59 +0600

    [Backend] : PORT SETED

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
-const port = 3000;
+dotenv.config();
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
