696af216a1f588b962d3f39fffa711139125a54c
Pranto Deb <rajprit0m@outlook.com>
2025-03-15 23:22:11 +0600

    [Backend] : [Controller] : [Aurh] : login OK

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const userRegisterIntoDB = async (req, res) => {
       res.status(400).send({ success: false, message: "Invalid User Data" });
     }
 
-    res.send({
+    res.status(200).send({
       success: true,
       message: "User registered successfully.",
       data: {
@@ -49,6 +49,41 @@ export const userRegisterIntoDB = async (req, res) => {
         email: newUser.email,
         gender: newUser.gender,
         profileImage: newUser.profileImage,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ success: false, message: "something want wrong!", error });
+  }
+};
+
+export const loginUserIntoDB = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found" });
+    }
+    const isMatch = bcrypt.compareSync(password, user.password || "");
+    if (!isMatch) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Wrong password" });
+    }
+    createToken(user._id, res);
+    res.status(200).send({
+      success: true,
+      message: "User logged successfully.",
+      data: {
+        _id: user._id,
+        fullname: user.fullname,
+        username: user.username,
+        email: user.email,
+        gender: user.gender,
+        profileImage: user.profileImage,
       },
     });
   } catch (error) {
--- backend/Routes/auth.route.js
+++ backend/Routes/auth.route.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { userRegisterIntoDB } from "../Controllers/auth.controller.js";
+import {
+  loginUserIntoDB,
+  userRegisterIntoDB,
+} from "../Controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", userRegisterIntoDB);
+router.post("/login", loginUserIntoDB);
 
 export const AuthRoute = router;
