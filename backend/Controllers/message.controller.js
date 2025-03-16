b62965f49d6f9523ff643d6ee27f425f59579402
Pranto Deb <rajprit0m@outlook.com>
2025-03-16 16:09:13 +0600

    [Backend] : [Controller] : chats

--- backend/Controllers/message.controller.js
+++ backend/Controllers/message.controller.js
@@ -1,6 +1,40 @@
+import { Conversation } from "../Models/conversation.model.js";
+import { Message } from "../Models/message.model.js";
+
 export const sentMessage = async () => {
+  console.log(req.user);
   try {
-    
+    const { message } = request.body;
+    const { id: receiverId } = request.params;
+    const senderId = req.user._id;
+
+    let chats = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!chats) {
+      chats = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessages = new Message({
+      senderId,
+      receiverId,
+      message,
+      conversationId: chats._id,
+    });
+
+    if (newMessages) {
+      chats.push(newMessages._id);
+    }
+
+    await Promise.all([chats.save(), newMessages.save()]);
+
+    // SOCKET.IO FUNCTIONS
+    res
+      .status(403)
+      .send({ success: false, message: "Outgoing Message....", newMessages });
   } catch (error) {
     console.error("Error sending message:", error);
   }
