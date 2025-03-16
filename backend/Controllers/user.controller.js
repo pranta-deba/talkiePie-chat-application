54a80afef9f82d1bce18691f6ca9049cc64f11c5
Pranto Deb <rajprit0m@outlook.com>
2025-03-16 17:06:26 +0600

    [Backend] : [Controller] : search users

--- backend/Controllers/user.controller.js
+++ backend/Controllers/user.controller.js
@@ -1,1 +1,36 @@
-export const getUserBySearch = async () => {};
+import { User } from "../Models/user.model.js";
+
+export const getUserBySearch = async (req, res) => {
+  try {
+    const search = req?.query?.search || "";
+    const currentUserId = req.user._id;
+
+    const users = await User.find({
+      $and: [
+        {
+          $or: [
+            { username: { $regex: search, $options: "i" } },
+            { fullname: { $regex: search, $options: "i" } },
+          ],
+        },
+        {
+          _id: { $ne: currentUserId },
+        },
+      ],
+    })
+      .select("-password")
+      .select("email");
+
+    res.json({
+      success: true,
+      message: "Users fetched successfully.",
+      data: users,
+    });
+    
+  } catch (error) {
+    console.error("Error get message:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "something went wrong!", error });
+  }
+};
