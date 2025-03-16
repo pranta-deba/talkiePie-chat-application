f1f5a9935e4ce8ee2f1ed37ac101fa867c807854
Pranto Deb <rajprit0m@outlook.com>
2025-03-16 17:34:42 +0600

    [Backend] : [Controller & Route] : current chatters OK

--- backend/Controllers/user.controller.js
+++ backend/Controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { Conversation } from "../Models/conversation.model.js";
 import { User } from "../Models/user.model.js";
 
 export const getUserBySearch = async (req, res) => {
@@ -36,6 +37,42 @@ export const getUserBySearch = async (req, res) => {
 
 export const getCurrentChatters = async (req, res) => {
   try {
+    const currentUserId = req.user._id;
+
+    const currentChatters = await Conversation.find({
+      participants: currentUserId,
+    }).sort({
+      updatedAt: -1,
+    });
+
+    if (!currentChatters || currentChatters.length === 0) {
+      return res
+        .status(200)
+        .send({ success: true, message: "No chats found.", data: [] });
+    }
+
+    const participantsIDs = currentChatters.reduce((ids, conversation) => {
+      const otherParticipants = conversation.participants.filter(
+        (id) => id !== currentUserId
+      );
+      return [...ids, ...otherParticipants];
+    }, []);
+    const otherParticipantsIDs = participantsIDs.filter(
+      (id) => id.toString() !== currentUserId.toString()
+    );
+
+    const user = await User.find({ _id: { $in: otherParticipantsIDs } })
+      .select("-password")
+      .select("-email");
+
+    const users = otherParticipantsIDs.map((id) =>
+      user.find((user) => user._id.toString() === id.toString())
+    );
+    res.json({
+      success: true,
+      message: "Chatter fetched successfully.",
+      data: users,
+    });
   } catch (error) {
     console.error("Error get message:", error);
     res
