ddd90aa1c7d1656c582453a68cb8c9baa6060072
Pranto Deb <rajprit0m@outlook.com>
2025-03-16 14:59:19 +0600

    [Backend] : [Controller] : send meaasge controller created

---
+++ backend/Controllers/message.controller.js
@@ -0,0 +1,7 @@
+export const sentMessage = async () => {
+  try {
+    
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+};
--- backend/Routes/message.route.js
+++ backend/Routes/message.route.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { sentMessage } from "../Controllers/message.controller.js";
 const router = express.Router();
 
-router.post("/send/:id");
+router.post("/send/:id", sentMessage);
 
 export const MessageRoute = router;
