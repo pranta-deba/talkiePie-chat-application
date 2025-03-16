87003a235cdae3e7c7a7b7acee9932ef93c2e260
Pranto Deb <rajprit0m@outlook.com>
2025-03-16 14:51:15 +0600

    [Backend] : [Model] : messages model

---
+++ backend/Models/messages.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
