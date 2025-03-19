a0ee4bcb3e784e0cd1c51f3fbc4aedb10eb03fd5
Pranto Deb <rajprit0m@outlook.com>
2025-03-19 22:06:17 +0600

    [Home] : apply responsive break point

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideBar from './Components/SideBar';
 import ChatContainer from './Components/ChatContainer';
 
 const Home = () => {
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+    const handelUserSelect = (user) => {
+        setSelectedUser(user);
+        setIsSidebarVisible(false);
+    }
+    const handelShowSidebar = () => {
+        setIsSidebarVisible(true);
+        setSelectedUser(null);
+    }
+
     return (
         <div className='min-h-screen w-full flex justify-center items-center gap-2 p-2'>
             {/* side bar */}
