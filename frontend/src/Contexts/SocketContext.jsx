d2f24e171379369aff41311dcc2c2cefbcd9955a
Pranto Deb <rajprit0m@outlook.com>
2025-03-21 00:14:32 +0600

    [Frontend] : [Socket Context] created

--- frontend/src/Contexts/SocketContext.jsx
+++ frontend/src/Contexts/SocketContext.jsx
@@ -1,1 +1,15 @@
+import { createContext, useContext, useState } from 'react';
 import io from 'socket.io-client';
+import { useAuth } from './AuthContext';
+
+export const SocketContext = createContext();
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+}
+
+export const SocketContextProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null);
+    const [onlineUser, setOnlineUser] = useState([]);
+    const { user } = useAuth();
+}
