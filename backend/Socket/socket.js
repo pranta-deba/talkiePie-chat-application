13f22496a7652193a7d094e9e47452e0e1afc231
Pranto Deb <rajprit0m@outlook.com>
2025-03-20 23:47:58 +0600

    [Backend] : [Socket] reciver socket id created and export

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -1,3 +1,19 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173/", "http://localhost:5174/"],
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap = {};
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
