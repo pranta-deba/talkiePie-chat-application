73a90341af67a41ba132514bafdf81e3ae57e4a4
Pranto Deb <rajprit0m@outlook.com>
2025-03-23 23:37:42 +0600

    [Backend] : frontend url added

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
