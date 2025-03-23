5d5e0680e9604de515873128419ed6210becd743
Pranto Deb <rajprit0m@outlook.com>
2025-03-24 00:00:29 +0600

    [Deployed Process] : frontend some changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://talkie-pie-client.vercel.app",
         secure: false,
       },
     },
